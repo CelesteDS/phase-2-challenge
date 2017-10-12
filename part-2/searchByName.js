9142a2e4dac7c563a6fb1e48b495a2da2c6264c3
Celeste De Santiago <celestedaphne@gmail.com>
2017-10-11 18:23:11 -0700

    Starting part 2

---
+++ part-2/searchByName.js
@@ -0,0 +1,2 @@
+// 40: searchByName.js script prints the client id and rep_name for all
+// clients whose rep_name starts with the provided name argument.
---
+++ part-2/searchByState.js
@@ -0,0 +1,2 @@
+// 40: searchByState.js script prints the client id, rep_name, company, city,
+// and state that for all clients whose state matches the provided state argument.
